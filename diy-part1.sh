852212fad7eddd1dc989f844aef5f9233e37779d
liu ta <34493169+acd889@users.noreply.github.com>
2024-09-29 19:05:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,7 +21,6 @@ echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.co
 rm -rf feeds/luci/applications/luci-app-passwall
 
 # 更换最新的golang版本
-rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
 
 # mosdns
